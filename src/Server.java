fb6e045c8d7fd2fdfdbf3a09199a6a11f852fb23
AlparslanBA <AlparslanBA58@hotmail.com>
2021-10-26 14:10:44 +0200

    Added a file, which can be read and write to

--- src/Server.java
+++ src/Server.java
@@ -1,11 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class Server {
+    File myObj = new File("publicFile.txt");
 
     public static void main(String[] args) throws RemoteException {
         Registry registry = LocateRegistry.createRegistry(6969);
         registry.rebind("communication", new Servant());
+    }
+
+    private void FileWriter(String input){
+        FileWriter myWriter;
+        try {
+            myWriter = new FileWriter(myObj);
+            myWriter.write(input);
+            myWriter.close();
+            System.out.println("File has been updated");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void FileReader() throws FileNotFoundException {
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
     }
 }
