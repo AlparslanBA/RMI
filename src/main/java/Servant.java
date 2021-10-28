a1583fc430af0189177559b5b0826fa5b58aafc4
Simon Pedersen <32456416+The-Hest@users.noreply.github.com>
2021-10-28 22:15:23 +0200

    Update Servant.java

--- src/main/java/Servant.java
+++ src/main/java/Servant.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Servant extends UnicastRemoteObject implements ServiceClass {
+public class Servant extends UnicastRemoteObject implements IServiceClass {
     public Servant() throws RemoteException {
     }
 
